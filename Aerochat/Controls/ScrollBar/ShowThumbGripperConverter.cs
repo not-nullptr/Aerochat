1684919245d427275ac457a5f2d1c2002b8b82b4
Isabella <kawapure@gmail.com>
2024-09-20 21:22:23 -0700

    feat: Accurate-to-WLM scroll bars
    This replaces the default Windows-Vista-style vector scrollbars provided by
    WPF. This uses the exact same 96-DPI images as Windows Live Messenger
    itself, and thus accounts for some of the subtle differences between the
    default Windows Vista Aero style and WLM (and the huge differences between
    the WPF vector and WLM :P).
    
    DPI variants don't exist yet, nor is there a vector fallback. I don't see
    this as being too much of an issue considering that Aerochat doesn't yet
    have any DPI variants of images.
    
    Signed-off-by: Isabella <kawapure@gmail.com>

---
+++ Aerochat/Controls/ScrollBar/ShowThumbGripperConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Aerochat.Controls.ScrollBar
+{
+    /// <summary>
+    /// Hack to not display the scrollbar gripper when the thumb is too small.
+    /// </summary>
+    [ValueConversion(typeof(Double), typeof(Boolean))]
+    internal class ShowThumbGripperConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo cultureInfo)
+        {
+            // This will be height for vertical scrollbars, and width for
+            // horizontal scrollbars.
+            double scrollbarSize = (double)value;
+
+            return scrollbarSize > 15;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Unhandled:
+            return Binding.DoNothing;
+        }
+    }
+}
