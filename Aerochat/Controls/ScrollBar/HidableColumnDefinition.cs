5ff2aa93604213d9ab43583cc7c04d428b958d85
Isabella <kawapure@gmail.com>
2024-09-26 22:00:13 -0700

    feat: Scroll bar jump-to-end buttons
    WLM 2009 featured a jump-to-bottom button seen on the scrollbar in chat
    windows, and contained bitmaps for other directions. This commit introduces
    the ability to optionally display one of these buttons on any scrollbar.
    
    The buttons are toggled through a simple attached property. Import the
    scrollbar namespace `Aerochat.Controls.ScrollBar` into your XAML file (i.e.
    as `scrollbar`), and then add a property to a ScrollBar control like
    `scrollbar:ScrollBarProperties.ShowJumpToBottom="True"` to display the
    button.
    
    The following properties are provided:
    - `ShowJumpToTop`
    - `ShowJumpToBottom`
    - `ShowJumpToLeft`
    - `ShowJumpToRight`
    
    Additionally, Chat.xaml has been modified to display the jump-to-bottom
    button in the main chat scroll view.
    
    Signed-off-by: Isabella <kawapure@gmail.com>

---
+++ Aerochat/Controls/ScrollBar/HidableColumnDefinition.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace Aerochat.Controls.ScrollBar
+{
+    /// <summary>
+    /// A column definition which can be hidden based on a condition.
+    /// </summary>
+    /// <see href="https://stackoverflow.com/a/30215839"/>
+    public class HidableColumnDefinition : ColumnDefinition
+    {
+        private GridLength _width;
+
+        public bool IsHidden
+        {
+            get { return (bool)GetValue(IsHiddenProperty); }
+            set { SetValue(IsHiddenProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsHiddenProperty =
+            DependencyProperty.Register(
+                "IsHidden", 
+                typeof(bool), 
+                typeof(HidableColumnDefinition), 
+                new PropertyMetadata(false, OnIsHiddenChanged)
+            );
+
+        public static void OnIsHiddenChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var o = d as HidableColumnDefinition;
+            o.Toggle((bool)e.NewValue);
+        }
+
+        public void Toggle(bool isHidden)
+        {
+            if (isHidden)
+            {
+                _width = Width;
+                Width = new GridLength(0, GridUnitType.Star);
+            }
+            else
+                Width = _width;
+        }
+    }
+}
---
+++ Aerochat/Converter/InvertBooleanConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Aerochat.Converter
+{
+    /// <summary>
+    /// Inverts a boolean value.
+    /// </summary>
+    /// <see href="https://stackoverflow.com/a/1039681" />
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class InverseBooleanConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+}
