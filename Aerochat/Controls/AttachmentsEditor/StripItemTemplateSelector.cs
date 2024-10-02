838065ace6d3a13d0f733cc18c1f0a7ce8897024
Isabella <kawapure@gmail.com>
2024-10-02 08:38:57 -0700

    feat: Introduce attachments editor GUI (unfinished)
    Signed-off-by: Isabella <kawapure@gmail.com>

---
+++ Aerochat/Controls/AttachmentsEditor/StripItemTemplateSelector.cs
@@ -0,0 +1,40 @@
+﻿using Aerochat.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Aerochat.Controls.AttachmentsEditor
+{
+    public class StripItemTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate? SelectTemplate(object item, DependencyObject container)
+        {
+            FrameworkElement? containerEl = container as FrameworkElement;
+
+            if (containerEl == null)
+            {
+                return null;
+            }
+
+            if (item == null || !(item is AttachmentsEditorItem))
+            {
+                throw new ApplicationException("Wrong type");
+            }
+
+            if ((item as AttachmentsEditorItem).IsImage)
+            {
+                return containerEl.FindResource("ImageItemContent") as DataTemplate;
+            }
+            else
+            {
+                return containerEl.FindResource("FileItemContent") as DataTemplate;
+            }
+
+            throw new ApplicationException("Failed to select valid template.");
+        }
+    }
+}
