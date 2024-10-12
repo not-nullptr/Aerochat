8e0793805f85f826ed0014022f2e4f6b720ba89a
Thomas May <wamwoowam@gmail.com>
2024-10-12 15:27:01 +0100

    small optimisation

--- DSharpPlus/Clients/DiscordClient.Dispatch.cs
+++ DSharpPlus/Clients/DiscordClient.Dispatch.cs
@@ -672,6 +672,7 @@ namespace DSharpPlus
                         xo._channel_id = xc.Id;
                     }
                 }
+
                 foreach (var (_, xt) in guild.Threads)
                 {
                     xt.GuildId = guild.Id;
@@ -939,7 +940,7 @@ namespace DSharpPlus
                 readStateDict.Add(state.Id, state);
             }
 
-            var ev = new ChannelUnreadUpdateEventArgs() { GuildId = guildId, ReadStates = readStateDict };
+            var ev = new ChannelUnreadUpdateEventArgs() { GuildId = guildId, ReadStates = readStateDict.ToFrozenDictionary() };
             await this._channelUnreadUpdate.InvokeAsync(this, ev);
         }
 
