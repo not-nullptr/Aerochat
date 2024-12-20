0f2315c725cc62b4c0e6f69bd9cc971bba859bc3
iLollek <78511903+iLollek@users.noreply.github.com>
2024-12-20 20:12:20 +0100

    feat: Option to Hide Community Images (#25) (#59)
    * feat: Option to Hide Community Images (#25)
    
    * feat: Removed the need of restart for the setting (PR #59)
    
    * fix(SettingsManager): Improve disable ads title
    
    I believe that this should be more clear to general users.
    
    Signed-off-by: Isabella Lulamoon <kawapure@gmail.com>
    
    ---------
    
    Signed-off-by: Isabella Lulamoon <kawapure@gmail.com>
    Co-authored-by: isabella lulamoon <kawapure@gmail.com>

---
+++ Aerochat/Converter/BooleanToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Aerochat.Windows
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility visibilityValue)
+            {
+                return visibilityValue == Visibility.Visible;
+            }
+            return false;
+        }
+    }
+}
