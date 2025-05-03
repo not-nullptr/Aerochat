11f2bf8f32843f6d1426df3627f7f4bb72bd2072
Isabella Lulamoon <kawapure@gmail.com>
2025-05-02 19:31:50 -0700

    feat: Ad information on about window and rewording
    Signed-off-by: Isabella Lulamoon <kawapure@gmail.com>

--- Aerochat/Windows/About.xaml.cs
+++ Aerochat/Windows/About.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using Aerochat.Theme;
+using Aerochat.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +15,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace Aerochat.Windows
 {
@@ -20,13 +24,41 @@ namespace Aerochat.Windows
         public About()
         {
             InitializeComponent();
-            // get all scenes
-            var scenes = ThemeService.Instance.Scenes;
+
             string credits = "Aerochat is a project by nullptr. Most assets belong to Microsoft, please don't sue!\n\n";
+
+            // Get all scenes:
+            credits += "==== Credits for scenes ==== \n\n";
+            var scenes = ThemeService.Instance.Scenes;
             foreach (var scene in scenes)
             {
                 credits += $"\"{scene.DisplayName}\" was made by {scene.Credit}\n";
             }
+            credits += "\n\n";
+
+            // Get all ads:
+            // TODO: Refactor into an ads service. Out of laziness, I just copied this code from the home page
+            // view model.
+            List<XElement> ads = new();
+
+            var assembly = Assembly.GetExecutingAssembly();
+            string resourceName = "Aerochat.Ads.Ads.xml";
+            using Stream stream = assembly.GetManifestResourceStream(resourceName);
+            using StreamReader reader = new(stream);
+            string result = reader.ReadToEnd();
+            XDocument doc = XDocument.Parse(result);
+            foreach (XElement adXml in doc.Root?.Elements() ?? [])
+            {
+                ads.Add(adXml);
+            }
+
+            credits += "==== Credits for ads ==== \n\n";
+            foreach (var ad in ads) if (ad.Attribute("Image") is not null)
+            {
+                credits += $"\"{ad.Attribute("Image")?.Value}\" was made by {ad.Attribute("Submitter")?.Value ?? "an unknown author. Please contact us if you made this ad."}\n";
+            }
+            // This is the group, so do not put the newlines.
+            //credits += "\n\n";
 
             CreditsTextbox.Text = credits;
         }
