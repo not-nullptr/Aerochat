0491f7754377f5e5de26ef9919794aad312ed856
Isabella Lulamoon <kawapure@gmail.com>
2025-06-07 16:20:32 -0700

    feat: Multi-boolean converter
    This is a useful utility.
    
    Signed-off-by: Isabella Lulamoon <kawapure@gmail.com>

---
+++ Aerochat/Converter/MultiBooleanConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Aerochat.Converter
+{
+    /// <summary>
+    /// Converts a set of booleans to a single boolean result. This will output true if all inputs are
+    /// true, or false otherwise.
+    /// </summary>
+    internal class MultiBooleanConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !values.Contains(false);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return [false];
+        }
+    }
+}
