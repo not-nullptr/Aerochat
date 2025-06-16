2f3e6df4457a0afdeb04806d35eead6c9914ff3c
Isabella Lulamoon <kawapure@gmail.com>
2025-06-16 16:04:29 -0700

    refactor: Use Vanara P/Invoke definitions in ProgramIconManager
    Signed-off-by: Isabella Lulamoon <kawapure@gmail.com>

--- Aerochat/Helpers/AttachmentEditor/ProgramIconManager.cs
+++ Aerochat/Helpers/AttachmentEditor/ProgramIconManager.cs
@@ -10,6 +10,8 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Interop;
 using System.Windows.Media.Imaging;
+using static Vanara.PInvoke.User32;
+using static Vanara.PInvoke.Shell32;
 
 namespace Aerochat.Helpers.AttachmentEditor
 {
@@ -27,14 +29,14 @@ namespace Aerochat.Helpers.AttachmentEditor
             try
             {
                 IntPtr hImgSmall = SHGetFileInfo(
-                    filePath, 0, ref fileInfo, (uint)Marshal.SizeOf(typeof(SHFILEINFO)),
-                    (uint)(SHGFI.Icon | SHGFI.UseFileAttributes | SHGFI.LargeIcon)
+                    filePath, 0, ref fileInfo, Marshal.SizeOf(typeof(SHFILEINFO)),
+                    SHGFI.SHGFI_ICON | SHGFI.SHGFI_USEFILEATTRIBUTES | SHGFI.SHGFI_LARGEICON
                 );
 
                 if (fileInfo.hIcon != 0)
                 {
                     BitmapSource result = Imaging.CreateBitmapSourceFromHIcon(
-                        fileInfo.hIcon, Int32Rect.Empty, BitmapSizeOptions.FromWidthAndHeight(32, 32)
+                        (nint)fileInfo.hIcon, Int32Rect.Empty, BitmapSizeOptions.FromWidthAndHeight(32, 32)
                     );
 
                     return result;
@@ -50,64 +52,5 @@ namespace Aerochat.Helpers.AttachmentEditor
                 }
             }
         }
-
-        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
-        struct SHFILEINFO
-        {
-            public IntPtr hIcon;
-            public int iIcon;
-            public uint dwAttributes;
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
-            public string szDisplayName;
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
-            public string szTypeName;
-        }
-
-        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
-        static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbFileInfo, uint uFlags);
-
-        [Flags]
-        enum SHGFI : int
-        {
-            /// <summary>get icon</summary>
-            Icon = 0x000000100,
-            /// <summary>get display name</summary>
-            DisplayName = 0x000000200,
-            /// <summary>get type name</summary>
-            TypeName = 0x000000400,
-            /// <summary>get attributes</summary>
-            Attributes = 0x000000800,
-            /// <summary>get icon location</summary>
-            IconLocation = 0x000001000,
-            /// <summary>return exe type</summary>
-            ExeType = 0x000002000,
-            /// <summary>get system icon index</summary>
-            SysIconIndex = 0x000004000,
-            /// <summary>put a link overlay on icon</summary>
-            LinkOverlay = 0x000008000,
-            /// <summary>show icon in selected state</summary>
-            Selected = 0x000010000,
-            /// <summary>get only specified attributes</summary>
-            Attr_Specified = 0x000020000,
-            /// <summary>get large icon</summary>
-            LargeIcon = 0x000000000,
-            /// <summary>get small icon</summary>
-            SmallIcon = 0x000000001,
-            /// <summary>get open icon</summary>
-            OpenIcon = 0x000000002,
-            /// <summary>get shell size icon</summary>
-            ShellIconSize = 0x000000004,
-            /// <summary>pszPath is a pidl</summary>
-            PIDL = 0x000000008,
-            /// <summary>use passed dwFileAttribute</summary>
-            UseFileAttributes = 0x000000010,
-            /// <summary>apply the appropriate overlays</summary>
-            AddOverlays = 0x000000020,
-            /// <summary>Get the index of the overlay in the upper 8 bits of the iIcon</summary>
-            OverlayIndex = 0x000000040,
-        }
-
-        [DllImport("user32.dll", SetLastError = true)]
-        extern static bool DestroyIcon(IntPtr hIcon);
     }
 }
